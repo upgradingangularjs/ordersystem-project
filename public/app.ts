a31619cc19b75c3b16f9b867e1ccf45b3df3ecf1
Sam Julien <spjulien@gmail.com>
2017-11-28 16:14:59 -0800

    Ready for course 2 module 3

--- public/app.ts
+++ public/app.ts
@@ -1,3 +1,6 @@
+import * as angular from 'angular';
+import 'angular-route';
+
 import homeComponent from './home/home';
 
 angular.module('app', ['ngRoute'])
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-    entry: './app.js',
+    entry: './app.ts',
     output: {
         filename: './dist/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
     }
 };

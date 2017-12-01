0deb6a951680bbeca125e3bf747c6cb05d6b84e7
Sam Julien <spjulien@gmail.com>
2017-11-30 16:25:02 -0800

    Starting point for navigation test video

---
+++ public/test/navigation.spec.js
@@ -0,0 +1,20 @@
+import 'angular';
+import 'angular-mocks';
+import '../app';
+
+describe('navigation component', function () {
+    var $componentController, ctrl;
+
+    beforeEach(function () {
+        angular.mock.module('app');
+
+        angular.mock.inject(function (_$componentController_) {
+            $componentController = _$componentController_;
+            ctrl = $componentController('navigation', null, {});
+        });
+    });
+
+    it('should have the correct company name', function () {
+        expect(ctrl.companyName).toEqual('Awesome, Inc.');
+    });
+}); 
